87d4317bdec0cbaf9fd47f76086027c9b9898416
Vaulry <marielaure-974@hotmail.fr>
2025-06-02 10:56:16 +0400

    evaluation

---
+++ migrations/Version20250602061823.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20250602061823 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql(<<<'SQL'
+            ALTER TABLE task CHANGE title title VARCHAR(255) NOT NULL, CHANGE description description LONGTEXT DEFAULT NULL, CHANGE status status VARCHAR(50) NOT NULL
+        SQL);
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql(<<<'SQL'
+            ALTER TABLE task CHANGE title title VARCHAR(255) DEFAULT NULL, CHANGE description description LONGTEXT NOT NULL, CHANGE status status VARCHAR(50) DEFAULT NULL
+        SQL);
+    }
+}
---
+++ src/Form/TaskType.php
@@ -0,0 +1,114 @@
+<?php
+namespace App\Form;
+use App\Entity\Task;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\Email;
+use Symfony\Component\Validator\Constraints\Length;
+use Symfony\Component\Validator\Constraints\NotBlank;
+class TaskType extends AbstractType
+{
+public function buildForm(FormBuilderInterface $builder, array $options):void
+{ $builder ->add(
+'title'
+, TextType::class, [
+'label'
+=>
+'Titre'
+,
+'attr'
+=> [
+'placeholder'
+=>
+'Saisir le titre'
+,
+'maxlength'
+=>
+100
+],
+'constraints'
+=> [
+new
+NotBlank([
+'message'
+=>
+'Le Titre est obligatoire'
+]),
+new
+Length([
+'max'
+=>
+100
+,
+'maxMessage'
+=>
+'Le titre ne peut dépasser {{ limit }} caractères'
+]) ] ]) 
+
+
+->add(
+'description'
+, TextType::class, [
+'label'
+=>
+'Description'
+,
+'attr'
+=> [
+'placeholder'
+=>
+'Saisir la description'
+,
+'maxlength'
+=>
+1000,
+
+],'constraints'
+=> [
+new
+NotBlank([
+'message'
+=>
+'La description est obligatoire'
+]),
+new
+Length([
+'max'
+=>
+100
+,
+'maxMessage'
+=>
+'La dexcription ne peut dépasser {{ limit }} caractères'
+]) ] ]) 
+
+
+->add(
+'status'
+, TextType::class, [
+'label'
+=>
+'Description'
+,
+'attr'
+=> [
+'placeholder'
+=>
+'Saisir la description'
+,
+'maxlength'
+=>
+100,
+
+ ]]);
+} 
+public function configureOptions
+(OptionsResolver $resolver)
+:
+void
+{ $resolver->setDefaults([
+'data_class'
+=> Task::class, ]); }}
